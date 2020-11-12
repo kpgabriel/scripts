540c4eca20589de04aa1c2593dd791f2d8bd8785
Kyle Gabriel <kpgabriel17@gmail.com>
2020-11-11 20:34:26 -0500

    adding function to process 2 files

--- python/Company_10-Q_Filings.py
+++ python/Company_10-Q_Filings.py
@@ -5,19 +5,29 @@ from datetime import date
 import re
 import io
 
-try:
-    # Make a request
-    cik_url='https://www.sec.gov/cgi-bin/current?q1=0&q2=1&q3='
-    source = requests.get(cik_url, allow_redirects=True).text
+
+def CreatTextFile(url):
+    today = date.today()
+    source = requests.get(url, allow_redirects=True).text
 
     soup = BeautifulSoup(source,'lxml')
     pre = soup.find('pre')
+
+    form_pattern = re.compile('(10-K|10-Q)')
+    form_name = re.search(form_pattern,pre.text)
+    
     pattern = re.compile('(?<=Company Name).?')
     formated_text = re.sub(pattern, "\n1", pre.text)
-    today = date.today()
-    
-    fname = 'C:\Projects\Reports\\10Q CIK Filings\{}_{}_{}_10QFilings'.format(today.year,today.month,today.day)
+    fname = 'C:\Projects\Reports\\{} Filings\{}_{}_{}_CompanyFilings'.format(form_name.group(0),today.year,today.month,today.day)
+
     with open(fname,'w', encoding="utf-8") as f:
         f.write(formated_text)
+
+try:
+    # Make a request
+    urls = ['https://www.sec.gov/cgi-bin/current?q1=0&q2=0&q3=','https://www.sec.gov/cgi-bin/current?q1=0&q2=1&q3=']
+    for url in urls:
+        CreatTextFile(url)
+    
 except Exception as e:
     print(e)
--- python/GetFinancialReports_10-Q.py
+++ python/GetFinancialReports_10-Q.py
@@ -2,61 +2,74 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from datetime import date
+import time
+
+
+def getFile(today):
+    ret_list = []
+    files = ['Q','K']
+    for i in range(len(files)):
+        fname = 'C:\Projects\Reports\\10-{} Filings\{}_{}_{}_CompanyFilings'.format(files[i],today.year,today.month,today.day)
+        ret_list.insert(i,fname)
+    return ret_list
+
 
 
 try:
     # Get Ciks from file of today
-    cik_dict = {}
+    today = date.today()
+    files = getFile(today)
 
-    today = date.today()    
-    fname = 'C:\Projects\Reports\\10Q CIK Filings\{}_{}_{}_10QFilings'.format(today.year,today.month,today.day)
-    file_cik = open(fname,'r')
-    for line in file_cik:
-        line_search = line.split()
-        name = ' '.join(line_search[3:])
+    for file in files:
+        form = re.search('(10-K|10-Q)',file)
+        cik_dict = {}
+        file_cik = open(file,'r')
+        for line in file_cik:
+            line_search = line.split()
+            name = ' '.join(line_search[3:])
+            company_name = re.sub('[^a-zA-Z0-9 \n\.]', '', name)
+            cik_dict[line_search[2]] = company_name
+            # print(company_name)
         
-        company_name = re.sub('[^a-zA-Z0-9 \n\.]', '', name)
-        cik_dict[line_search[2]] = company_name
-        # print(company_name)
+        for cik_value, company  in cik_dict.items(): 
+            # Make a request
+            if cik_value == 'Form':
+                pass
+            else:
+                url = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK={}'.format(cik_value)
+                source = requests.get(url, allow_redirects=True).text
+                soup = BeautifulSoup(source,'lxml')
 
-    for cik_value, company  in cik_dict.items(): 
-        # Make a request
-        if cik_value == 'Form':
-            pass
-        else:
-            url = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK={}'.format(cik_value)
-            source = requests.get(url, allow_redirects=True).text
-            soup = BeautifulSoup(source,'lxml')
+                # Searching the html tags
+                text_to_search = soup.find(id='seriesDiv')
+                table_body = text_to_search.find('table')
+                table_row_data = table_body.find_all('td')
 
-            # Searching the html tags
-            text_to_search = soup.find(id='seriesDiv')
-            table_body = text_to_search.find('table')
-            table_row_data = table_body.find_all('td')
-
-            # regex for finding the account number associated with the 10-Q report
-            quarterly_report = re.compile('(?<=Quarterly report).+?(?=(34 Act))')
-            acc_id = re.compile('(?<=Acc-no:).[0-9]+-[0-9]+-[0-9]+')
-        
-            match = re.search(quarterly_report,str(table_row_data))
-        
-            if match:
-                
-                acc_no = re.search(acc_id,match.group(0))
-                if acc_no:
-                    # Clean up the account number
-                    number = acc_no.group(0).split()
-                    truncated_acc_number = number[0].replace('-','')
-                    # Get the report
-                    financial_report_url = 'https://www.sec.gov/Archives/edgar/data/{0}/{1}/Financial_Report.xlsx'.format(cik_value,truncated_acc_number)
-                    source = requests.get(financial_report_url, allow_redirects=True)
-                    if today.month <= 3 and today.day <= 31:
-                        open('C:\Projects\Reports\{}\Q1\Financial_Report_{}.xlsx'.format(today.year,company),'wb').write(source.content)    
-                    elif today.month <= 6 and today.day <= 30:
-                        open('C:\Projects\Reports\{}\Q2\Financial_Report_{}.xlsx'.format(today.year,company),'wb').write(source.content)    
-                    elif today.month <= 9 and today.day <= 30:
-                        open('C:\Projects\Reports\{}\Q3\Financial_Report_{}.xlsx'.format(today.year,company),'wb').write(source.content)    
-                    else:
-                        open('C:\Projects\Reports\{}\Q4\Financial_Report_{}.xlsx'.format(today.year,company),'wb').write(source.content)    
+                # regex for finding the account number associated with the 10-Q report
+                quarterly_report = re.compile('(?<=Quarterly report).+?(?=(34 Act))')
+                acc_id = re.compile('(?<=Acc-no:).[0-9]+-[0-9]+-[0-9]+')
+            
+                match = re.search(quarterly_report,str(table_row_data))
+            
+                if match:
                     
+                    acc_no = re.search(acc_id,match.group(0))
+                    if acc_no:
+                        # Clean up the account number
+                        number = acc_no.group(0).split()
+                        truncated_acc_number = number[0].replace('-','')
+                        # Get the report
+                        financial_report_url = 'https://www.sec.gov/Archives/edgar/data/{0}/{1}/Financial_Report.xlsx'.format(cik_value,truncated_acc_number)
+                        source = requests.get(financial_report_url, allow_redirects=True)
+                        if today.month <= 3 and today.day <= 31:
+                            open('C:\Projects\Reports\{}\Q1\\{}\Financial_Report_{}.xlsx'.format(today.year,form.group(0),company),'wb').write(source.content)    
+                        elif today.month <= 6 and today.day <= 30:
+                            open('C:\Projects\Reports\{}\Q2\\{}\Financial_Report_{}.xlsx'.format(today.year,form.group(0),company),'wb').write(source.content)    
+                        elif today.month <= 9 and today.day <= 30:
+                            open('C:\Projects\Reports\{}\Q3\\{}\Financial_Report_{}.xlsx'.format(today.year,form.group(0),company),'wb').write(source.content)    
+                        else:
+                            open('C:\Projects\Reports\{}\Q4\\{}\Financial_Report_{}.xlsx'.format(today.year,form.group(0),company),'wb').write(source.content) 
+                time.sleep(.1)   
+                        
 except Exception as e:
     print(e)
