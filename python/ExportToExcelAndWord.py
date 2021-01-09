ca7c9f473d01ed75be9b618b9d6eff3a26602e4c
Kyle Gabriel <kpgabriel17@gmail.com>
2021-01-09 16:20:51 -0500

    Adding formatiing to excel export

--- python/ExportToExcelAndWord.py
+++ python/ExportToExcelAndWord.py
@@ -5,44 +5,57 @@ from xlsxwriter.utility import xl_rowcol_to_cell
 
 
 try:
-    xls = pd.ExcelFile('C:\Projects\Reports\\2020\Q4\\10-K\Financial_Report_Quanex Building Products CORP.xlsx')
+    xls = pd.ExcelFile('C:\Projects\Reports\\2021\Q1\\10-K\Financial_Report_Guskin Gold Corp..xlsx')
     form_pattern = re.compile('(Consolidated|Statements)')
     sheets = xls.sheet_names
     newlist = list(filter(form_pattern.match, sheets))
     newlist.insert(0,sheets[0])
-    df = pd.read_excel('C:\Projects\Reports\\2020\Q4\\10-K\Financial_Report_Quanex Building Products CORP.xlsx',sheet_name=newlist)
-    # print(newlist)
-    coverpage          = df[newlist[0]]
-    balancesheet1      = df[newlist[1]]
-    balancesheet2      = df[newlist[2]]
-    statementofincome  = df[newlist[3]]
-    statementofcomp    = df[newlist[4]]
-    statementofstock1  = df[newlist[5]]
-    statementofstock2  = df[newlist[6]]
-    statementofcash    = df[newlist[7]]
+    dict_sheets = pd.read_excel('C:\Projects\Reports\\2021\Q1\\10-K\Financial_Report_Guskin Gold Corp..xlsx',sheet_name=newlist)
+    print(newlist)
 
-    coverpage_df          = pd.DataFrame(coverpage)
-    balancesheet1_df      = pd.DataFrame(balancesheet1)
-    balancesheet2_df      = pd.DataFrame(balancesheet2)
-    statementofincome_df  = pd.DataFrame(statementofincome)
-    statementofcomp_df    = pd.DataFrame(statementofcomp)
-    statementofstock1_df  = pd.DataFrame(statementofstock1)
-    statementofstock2_df  = pd.DataFrame(statementofstock2)
-    statementofcash_df    = pd.DataFrame(statementofcash)
+    income_sheets = {}
+    for x in range(len(newlist)):
+        # Create the dictionary of data fram objects with sheet names
+        income_sheets['{0}'.format(newlist[x])] =  pd.DataFrame(dict_sheets[newlist[x]])
 
+    # Make a writer using xlsxwriter library
+    writer_orig = pd.ExcelWriter('C:/Projects/Reports/simple.xlsx', engine='xlsxwriter')
 
-    # print(type(newdf))
-    # df.to_excel('C:\Projects\Reports\\2020\Q4\\10-K\temp\Financial_Report_Quanex Building Products CORP.xlsx')   
-    income_sheets = {'coverpage': coverpage_df, 'balancesheet1': balancesheet1_df, 'balancesheet2': balancesheet2_df, 
-                    'statementofincome': statementofincome_df,'statementofcomp': statementofcomp_df,'statementofstock1': statementofstock1_df,
-                    'statementofstock2': statementofstock2_df,'statementofcash':statementofcash_df}
-
-    writer = pd.ExcelWriter('C:/Projects/Reports/2020/Q4/10-K/temp/Financial_Report_Quanex Building Products CORP.xlsx', engine='xlsxwriter')
+    # Simple excel document
+    for sheet_name in income_sheets.keys():
+        income_sheets[sheet_name].to_excel(writer_orig, sheet_name=sheet_name, index=False)
     
+    writer_orig.save()
+
+
+
+    # Now we format and write a more complicated output
+
+
+
+
+    writer = pd.ExcelWriter('C:/Projects/Reports/fancy.xlsx', engine='xlsxwriter')
+    # Get the workbook
+    workbook = writer.book
+
+    # Add a bold format to use to highlight cells.
+    bold = workbook.add_format({'bold': True})
+    # Add a custom format for cells with money.
+    money = workbook.add_format({'num_format': '_("$ "#,##0_);_("$ "(#,##0)'})
+
+    num_sheet = 0
     for sheet_name in income_sheets.keys():
         income_sheets[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
 
+        # Get the worksheet
+        worksheet = writer.sheets[sheet_name]
+        # Change the cell columns in all sheets
+        
+        worksheet.set_column('A:A', 40)
+        worksheet.set_column('B:B', 25)
+        worksheet.set_column('C:C', 25)
+        
     writer.save()
-    
+
 except Exception as e:
     print(e)
