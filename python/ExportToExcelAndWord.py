8e91bcd423ea30a3c34d3b9a9b5151a3a4fb9b6a
Kyle Gabriel <kpgabriel17@gmail.com>
2021-01-21 19:00:29 -0500

    Adding dictionary items to word

--- python/ExportToExcelAndWord.py
+++ python/ExportToExcelAndWord.py
@@ -76,43 +76,44 @@ try:
 
     # Now we try writing to word 
     document = Document()
-    document.add_heading('{0}'.format(other_sheets[0]), 0)
+    for i in range(len(other_sheets)): 
+        document.add_heading('{0}'.format(other_sheets[i]), 0)
 
-    p = document.add_paragraph('{0}'.format(other_sheets_dict[other_sheets[0]]))
+        p = document.add_paragraph('{0}'.format(other_sheets_dict[other_sheets[i]]))
     # p.add_run('bold').bold = True
     # p.add_run(' and some ')
     # p.add_run('italic.').italic = True
 
-    document.add_heading('Heading, level 1', level=1)
-    document.add_paragraph('Intense quote', style='Intense Quote')
+    # document.add_heading('Heading, level 1', level=1)
+    # document.add_paragraph('Intense quote', style='Intense Quote')
 
-    document.add_paragraph(
-        'first item in unordered list', style='List Bullet'
-    )
-    document.add_paragraph(
-        'first item in ordered list', style='List Number'
-    )
+    # document.add_paragraph(
+    #     'first item in unordered list', style='List Bullet'
+    # )
+    # document.add_paragraph(
+    #     'first item in ordered list', style='List Number'
+    # )
 
     # document.add_picture('monty-truth.png', width=Inches(1.25))
 
-    records = (
-        (3, '101', 'Spam'),
-        (7, '422', 'Eggs'),
-        (4, '631', 'Spam, spam, eggs, and spam')
-    )
+    # records = (
+    #     (3, '101', 'Spam'),
+    #     (7, '422', 'Eggs'),
+    #     (4, '631', 'Spam, spam, eggs, and spam')
+    # )
 
-    table = document.add_table(rows=1, cols=3)
-    hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'Qty'
-    hdr_cells[1].text = 'Id'
-    hdr_cells[2].text = 'Desc'
-    for qty, id, desc in records:
-        row_cells = table.add_row().cells
-        row_cells[0].text = str(qty)
-        row_cells[1].text = id
-        row_cells[2].text = desc
+    # table = document.add_table(rows=1, cols=3)
+    # hdr_cells = table.rows[0].cells
+    # hdr_cells[0].text = 'Qty'
+    # hdr_cells[1].text = 'Id'
+    # hdr_cells[2].text = 'Desc'
+    # for qty, id, desc in records:
+    #     row_cells = table.add_row().cells
+    #     row_cells[0].text = str(qty)
+    #     row_cells[1].text = id
+    #     row_cells[2].text = desc
 
-    document.add_page_break()
+        document.add_page_break()
 
     document.save('C:/Projects/Reports/demo.docx')
 
